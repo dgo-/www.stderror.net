8e33770b27efc5f4660e30cf63a4da865e08e8d7
Daniel <dgo-@users.noreply.github.com>
2018-08-21 08:33:14 +0200

    no preserve rights

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 hugo --quiet
 
 #deploy
-s3cmd sync  public/ s3://www.stderror.net --exclude '.DS_Store' --delete-removed
+s3cmd sync  public/ s3://www.stderror.net --exclude '.DS_Store' --delete-removed --no-preserve
